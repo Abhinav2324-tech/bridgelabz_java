d372847bdd9c692b5ee55bfa9732ea28eb04fae2
Abhinav2324-tech <abhinavgoud2324@gmail.com>
2026-08-12 16:38:32 +0530

    [Abhinav]:Solved few questions from Level 1 of java_programming_element

---
+++ src/com/bridgelabz/java_programming_element/Level_1/Average.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.java_programming_element.Level_1;
+
+public class Average {
+    public static void main(String[] args){
+        int math=94;
+        int physics=95;
+        int chemistry=96;
+        double average=(math+physics+chemistry)/3.0;
+        System.out.println("Sam’s average mark in PCM is:"+average);
+    }
+}
---
+++ src/com/bridgelabz/java_programming_element/Level_1/Distance.java
@@ -0,0 +1,9 @@
+package com.bridgelabz.java_programming_element.Level_1;
+
+public class Distance {
+    public static void main(String[] args){
+        double kilometres=10.8;
+        double miles=(kilometres)/1.6;
+        System.out.println("The distance "+kilometres+"km in miles is:"+miles);
+    }
+}
